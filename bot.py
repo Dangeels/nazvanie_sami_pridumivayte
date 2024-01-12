90a0fb66fe333ff97acc70a0343f3e022e0b9910
Dangeels <114171101+Dangeels@users.noreply.github.com>
2024-01-12 23:12:45 +0300

    1

--- bot.py
+++ bot.py
@@ -2,18 +2,17 @@ import config
 import telebot
 from telebot import types
 from telebot.types import ReplyKeyboardMarkup,KeyboardButton, MenuButtonDefault
+import json
+import text
+
 
 bot = telebot.TeleBot(config.bot_token)
 
-
-
-
-
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start','help','referral'])
 def start(m):
-    c1 = types.BotCommand(command='start', description='Start the Bot')
+    c1 = types.BotCommand(command='myprofile', description='Start the Bot')
     c2 = types.BotCommand(command='help', description='Click for Help')
-    c3 = types.BotCommand(command='go', description='Something')
+    c3 = types.BotCommand(command='referral', description='Something')
     bot.set_my_commands([c1, c2, c3])
     bot.set_chat_menu_button(m.chat.id, types.MenuButtonCommands('commands'))
 
@@ -22,20 +21,19 @@ def start(m):
     next_btn = KeyboardButton('все понятно! пройти регистрацию')
     markup.add(next_btn)
 
-    text = f'Привет {m.from_user.first_name}!'
-    bot.send_message(m.chat.id, text)
-    bot.send_message(m.chat.id,text='fuck',reply_markup=markup)
+    x = telebot.util.extract_command(m.text)
+
+    bot.send_message(m.chat.id,text=text.text[x],reply_markup=markup)
+    bot.send_message(m.chat.id,text.text['1']) if x == 'start' else None
+
+questions = []
 
 
-class Anketa:
-    
-
-@bot.message_handler(regexp='все понятно! пройти регистрацию')
+@bot.message_handler(commands=['myprofile'])
 def profile(m):
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    next_btn = KeyboardButton('net')
-    markup.add(next_btn)
+    ...
 
-    bot.send_message(m.chat.id,text='123',reply_markup=markup)
+
+
 
 bot.infinity_polling()
