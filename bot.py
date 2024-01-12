fdd703b326465596c433c43c15d514cef4e545d9
Dangeels <114171101+Dangeels@users.noreply.github.com>
2024-01-13 00:31:36 +0300

    1

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import config
 import telebot
+from dataclasses import dataclass
 from telebot import types
 from telebot.types import ReplyKeyboardMarkup,KeyboardButton, MenuButtonDefault
 import json
@@ -18,7 +19,7 @@ def start(m):
 
 
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    next_btn = KeyboardButton('все понятно! пройти регистрацию')
+    next_btn = KeyboardButton('инструкция')
     markup.add(next_btn)
 
     x = telebot.util.extract_command(m.text)
@@ -26,7 +27,39 @@ def start(m):
     bot.send_message(m.chat.id,text=text.text[x],reply_markup=markup)
     bot.send_message(m.chat.id,text.text['1']) if x == 'start' else None
 
-questions = []
+@dataclass
+class Questions:
+    question: str
+
+questions = [
+    Questions('Сколько тебе лет?'),
+    Questions('Как тебя зовут(Имя Фамилия)?'),
+    Questions('В каком ты классе?'),         # Номер класса в сообщении, буква в кнопках
+    Questions('Отправь свою фотку')
+             ]
+
+@bot.message_handler(regexp='инструкция')
+def instruction(m):
+
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    btn = KeyboardButton('Подарки и бонусы')
+    markup.add(btn)
+
+    bot.send_message(m.chat.id,text.text['help'], reply_markup=markup)
+    bot.send_message(m.chat.id, text.text['ne_ebu'])
+@bot.message_handler(regexp='Подарки и бонусы')
+def ref(m):
+
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    btn = KeyboardButton('Пройти регистрацию')
+    markup.add(btn)
+
+    bot.send_message(m.chat.id, text.text['referral'],reply_markup=markup)
+
+@bot.message_handler(regexp='Пройти регистрацию')
+def registration(m):
+    bot.send_message(m.chat.id, text.text['important'])
+    bot.send_message(m.chat.id, text.text['anketa'])
 
 
 @bot.message_handler(commands=['myprofile'])
